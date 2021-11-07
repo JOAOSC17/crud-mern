8d1d47410fc499e567f121daadc8a80983e04819
João Costa <costav1402@gmail.com>
2021-11-07 11:48:29 -0300

    create front and remove update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [foodName, setFoodName] = useState("");
+  const [days, setDays] = useState(0);
+  const [newFoodName, setNewFoodName] = useState("");
+  const [foodList, setFoodList] = useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3001/read").then((response)=>{
+      console.log(response)
+      setFoodList(response.data)
+    })
+  },[])
+  const addToList = () => {
+    axios.post("http://localhost:3001/insert",{
+      foodName:foodName,
+      days:days
+    })
+    setFoodName("")
+    setDays(0)
+  }
+  const updateFood = (id) => {
+    console.log(id)
+    axios.put("http://localhost:3001/update",{
+      id:id,
+      newFoodName:newFoodName
+    })
+  }
+  const deleteFood = (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`)
+    console.log(id)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App with MERN</h1>
+      <label>Food Name:</label>
+      <input type="text" value={foodName} onChange={(ev)=>{
+        setFoodName(ev.target.value)
+      }}/>
+      <label>Day Since You Ate It:</label>
+      <input type="number" value={days} onChange={(ev)=>{
+        setDays(ev.target.value)
+      }}/>
+      <button onClick={addToList}>Add To List</button>
+      <h1>Food List</h1>
+      {foodList.map((val,key)=>{
+        return(
+          <div key={key} className="food">
+            <h1>{val.foodName}</h1><h1>{val.daySinceIAte}</h1>
+            <input type="text" placeholder="New Food name"onChange={(ev)=>{
+             setNewFoodName(ev.target.value)
+            }}/>
+            <button onClick={()=>updateFood(val._id)}>Update</button>
+            <button onClick={()=>deleteFood(val._id)}>Delete</button>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 const FoodModel = require("./models/Food")
 app.use(express.json());
+app.use(cors())
 mongoose.connect("mongodb+srv://joao:duque123@crud.xiq3c.mongodb.net/food?retryWrites=true&w=majority",{
     useNewUrlParser:true,
 });
 
-app.get("/", async (req, res) => {
-    const food = new FoodModel({foodName:"Apple", daySinceIAte:3});
+app.post("/insert", async (req, res) => {
+    const foodName = req.body.foodName;
+    const days = req.body.days;
+    const food = new FoodModel({foodName, daySinceIAte:days});
     try{
         await food.save();
         res.send("insert data")
@@ -16,6 +20,34 @@ app.get("/", async (req, res) => {
         console.log(err)
     }
 })
+app.get("/read", async (req, res) => {
+    FoodModel.find({}, (err, result)=>{
+        if(err){
+            res.send(err)
+        }
+        res.send(result)
+    })
+})
+app.put("/update", async (req, res) => {
+    const newFoodName = req.body.newFoodName;
+    const id = req.body.id;
+    try{
+        await FoodModel.findById(id, (err, updatedFood)=>{
+            updatedFood.foodName = newFoodName;
+            updatedFood.save();
+        });
+        res.send("update")
+    }catch(err){
+        console.log(err)
+    }
+})
+app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    await FoodModel.findByIdAndRemove(id).exec();
+    res.send("deleted");
+})
+
 app.listen(3001,()=>{
     console.log("Server is running on port 3001...");
 })
